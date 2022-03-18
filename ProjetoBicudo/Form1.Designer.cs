b7ee4bdb39048034cd95be4788361fe71d149f20
bruna-raisa <bruna.raissa2003@gmail.com>
2022-03-18 15:35:55 -0300

    meu primeiro commit

---
+++ ProjetoBicudo/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ProjetoBicudo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtzembra = new System.Windows.Forms.TextBox();
+            this.txtpeixe = new System.Windows.Forms.TextBox();
+            this.txtlili = new System.Windows.Forms.TextBox();
+            this.txtjoaninha = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btnchecar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(340, 9);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(262, 40);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "Ordene As Frases";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(344, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PEIXE LAGOA DOURADO NADA O NA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(343, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "A LISTRADO TEM CORPO O ZEBRA";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(45, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(257, 18);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "JOANINHA INSETO PEQUENO É UM A";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(45, 234);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(281, 18);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "BRINCA NA LILI AREIA COM O BALDE SEU";
+            // 
+            // txtzembra
+            // 
+            this.txtzembra.Location = new System.Drawing.Point(347, 117);
+            this.txtzembra.Name = "txtzembra";
+            this.txtzembra.Size = new System.Drawing.Size(258, 20);
+            this.txtzembra.TabIndex = 12;
+            // 
+            // txtpeixe
+            // 
+            this.txtpeixe.Location = new System.Drawing.Point(347, 201);
+            this.txtpeixe.Name = "txtpeixe";
+            this.txtpeixe.Size = new System.Drawing.Size(258, 20);
+            this.txtpeixe.TabIndex = 13;
+            // 
+            // txtlili
+            // 
+            this.txtlili.Location = new System.Drawing.Point(48, 281);
+            this.txtlili.Name = "txtlili";
+            this.txtlili.Size = new System.Drawing.Size(278, 20);
+            this.txtlili.TabIndex = 14;
+            // 
+            // txtjoaninha
+            // 
+            this.txtjoaninha.Location = new System.Drawing.Point(48, 376);
+            this.txtjoaninha.Name = "txtjoaninha";
+            this.txtjoaninha.Size = new System.Drawing.Size(278, 20);
+            this.txtjoaninha.TabIndex = 15;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(364, 270);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(241, 126);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(530, 415);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 17;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            // 
+            // btnchecar
+            // 
+            this.btnchecar.Location = new System.Drawing.Point(624, 415);
+            this.btnchecar.Name = "btnchecar";
+            this.btnchecar.Size = new System.Drawing.Size(75, 23);
+            this.btnchecar.TabIndex = 18;
+            this.btnchecar.Text = "Checar";
+            this.btnchecar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnchecar);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtjoaninha);
+            this.Controls.Add(this.txtlili);
+            this.Controls.Add(this.txtpeixe);
+            this.Controls.Add(this.txtzembra);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtzembra;
+        private System.Windows.Forms.TextBox txtpeixe;
+        private System.Windows.Forms.TextBox txtlili;
+        private System.Windows.Forms.TextBox txtjoaninha;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btnchecar;
+    }
+}
+
